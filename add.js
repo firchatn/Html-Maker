d7f7706716c44bce5f2d38c6e887fc8a33d0d990
firchtn <firaschaabencss@gmail.com>
2017-01-17 13:44:58 +0100

    first commit h Maker

---
+++ add.js
@@ -0,0 +1,77 @@
+function add_input_text(){
+	var para = document.createElement("input");
+	var node = document.createTextNode("This is new.");
+	para.appendChild(node);
+	var element = document.getElementById("div1");
+	element.appendChild(para);
+}
+function add_h1(){
+	var para = document.createElement("h1");
+	var node = document.createTextNode("This is new.");
+	para.appendChild(node);
+	var element = document.getElementById("div1");
+	element.appendChild(para);
+}
+function add_h2(){
+	var para = document.createElement("h2");
+	var node = document.createTextNode("This is new.");
+	para.appendChild(node);
+	var element = document.getElementById("div1");
+	element.appendChild(para);
+}
+function add_h3(){
+	var para = document.createElement("h3");
+	var node = document.createTextNode("This is new.");
+	para.appendChild(node);
+	var element = document.getElementById("div1");
+	element.appendChild(para);
+}
+function add_h4(){
+	var para = document.createElement("h4");
+	var node = document.createTextNode("This is new.");
+	para.appendChild(node);
+	var element = document.getElementById("div1");
+	element.appendChild(para);
+}
+function add_h5(){
+	var para = document.createElement("h5");
+	var node = document.createTextNode("This is new.");
+	para.appendChild(node);
+	var element = document.getElementById("div1");
+	element.appendChild(para);
+}
+function add_h6(){
+	var para = document.createElement("h6");
+	var node = document.createTextNode("This is new.");
+	para.appendChild(node);
+	var element = document.getElementById("div1");
+	element.appendChild(para);
+}
+
+
+function add_label_name(){
+	var para = document.createElement("label");
+	var node = document.createTextNode("Name : ");
+	para.appendChild(node);
+	var element = document.getElementById("div1");
+	element.appendChild(para);
+}
+function add_label_lastname(){
+	var para = document.createElement("label");
+	var node = document.createTextNode("Last name : ");
+	para.appendChild(node);
+	var element = document.getElementById("div1");
+	element.appendChild(para);
+	up_key();
+}
+function up_key(){
+	label.style.left = "300px";
+}
+function add_br(){
+	var para = document.createElement("br");
+	var node = document.createTextNode(" ");
+	para.appendChild(node);
+	var element = document.getElementById("div1");
+	element.appendChild(para);
+
+}
